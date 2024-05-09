0239dd48abfb0dce0ca8fba7fe5d799768c1efb6
ashasingh04 <asha.sgsits@gmail.com>
2024-05-09 17:27:16 -0500

    Tech jobs MVC version bonus mission code added

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -42,6 +42,7 @@ public class SearchController {
 
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("selectedColumn",searchType);
         return "search";
     }
 }
